1ca58d33643dc4cc2e1693d6ff4ae8eac727bcfb
vitor.rigolon <vrigolon@mail.com>
2020-01-22 19:34:41 +0000

    UI Changes

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ function newItem() {
   var item = document.getElementById("input").value;
   var ul = document.getElementById("list");
   var li = document.createElement("li");
-  li.appendChild(document.createTextNode("- " + item));
+  
+  li.appendChild(document.createTextNode(item.toUpperCase()));
   ul.appendChild(li);
   document.getElementById("input").value = "";
   li.onclick = removeItem;
